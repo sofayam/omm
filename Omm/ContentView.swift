44dc70b10e9d70383cd7bbd57c3bebc76559ac5a
mark andrew <mark@air.local>
2025-06-06 16:46:43 +0100

    Initial Commit

---
+++ Omm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Omm
+//
+//  Created by mark andrew on 06/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Omm/OmmApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmmApp.swift
+//  Omm
+//
+//  Created by mark andrew on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct OmmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
