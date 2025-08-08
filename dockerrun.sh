1c88546483370dbece25f252c77c586c24004a15
mark andrew <mark@air.local>
2025-08-08 15:55:18 +0100

    grr

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,3 +1,3 @@
 docker run -p 8020:8020 \
-  -v "/volume1/music/meditation/enonewspace:/usr/src/app/audio/music.mp3" \
+  -v "/volume1/music/meditation/enonewspace.mp3:/usr/src/app/audio/music.mp3" \
    omm-server
